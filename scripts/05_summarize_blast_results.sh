0d9411dfaf9e5a6711a57dc6011f9d0749133267
Shreya Barma <sbarma3@dons.usfca.edu>
2019-12-04 23:57:29 +0000

    Add script 5 with shellcheck edits

--- scripts/05_summarize_blast_results.sh
+++ scripts/05_summarize_blast_results.sh
@@ -1,9 +1,10 @@
-#! use cut, sort, and uniq -c to help you summarize the results from the
+#!/bin/bash
+#  use cut, sort, and uniq -c to help you summarize the results from the
 # BLAST search.
 for file in /data/illumina_sequences/blast_output/*.csv
 do
-	echo processing $file
-	cut -d, -f1 $file | sort | uniq -c | sort -n > /data/illumina_sequences/blast_results_summarized/$(basename -s .csv $file)_summarized.csv
+	echo processing "$file"
+	cut -d, -f1 "$file" | sort | uniq -c | sort -n > /data/illumina_sequences/blast_results_summarized/"$(basename -s .csv "$file")_summarized.csv"
 	echo Done
 done
 
