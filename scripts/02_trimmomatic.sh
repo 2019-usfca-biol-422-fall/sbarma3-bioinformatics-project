b620fc4d38f04f7f35a6cd86506746e5b8d3c140
Shreya Barma <sbarma3@dons.usfca.edu>
2019-12-04 23:56:07 +0000

    Add script 2 with shellcheck changes

--- scripts/02_trimmomatic.sh
+++ scripts/02_trimmomatic.sh
@@ -1,5 +1,11 @@
-#! run trimmomatic to throw out bad sequences, trim when quality gets low, or if
+#!/bin/bash
+# run trimmomatic to throw out bad sequences, trim when quality gets low, or if
 # sequences are too short you will need to turn this into a for loop to process
 # all of your files
-TrimmomaticSE -threads 4 -phred33 /data/illumina_sequences/unzipped/SB-SB_S28_L001_R1_001.fastq /data/illumina_sequences/trimmed/$(basename -s .fastq SB-SB_S28_L001_R1_001.fastq ).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:140
 
+for file in /data/illumina_sequences/unzipped/SB*.fastq 
+do
+	echo running trimmomatic on "$file"
+	TrimmomaticSE -threads 4 -phred33 "$file" /data/illumina_sequences/trimmed/"$(basename -s .fastq "$file")".trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:140
+	echo Done
+done 
