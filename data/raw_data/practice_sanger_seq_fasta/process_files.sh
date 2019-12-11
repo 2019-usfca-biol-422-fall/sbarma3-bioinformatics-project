e80ce9ca3f1229aebde57e9ef17c3c694c44d1a9
Shreya Barma <sbarma3@dons.usfca.edu>
2019-12-11 20:47:08 +0000

    Add travis edits to file

--- data/raw_data/practice_sanger_seq_fasta/process_files.sh
+++ data/raw_data/practice_sanger_seq_fasta/process_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# A sample script to process some files 
-# Shreya Barma 
-# October 23, 2019 
+# A sample script to process some files
+# Shreya Barma
+# October 23, 2019
 # sbarma3@dons.usfca.edu
 
 echo "Hello World"
@@ -10,24 +10,24 @@ echo "This is your script speaking"
 
 echo "Starting for loop..."
 
-#this is a for loop 
-for file in *file*.txt 
-do 
-	echo $file 
-done 
+#this is a for loop
+for file in *file*.txt
+do
+	echo "$file"
+done
 
-#here's a for loop with numbers 
+#here's a for loop with numbers
 for number in {1..10}
 do
-	echo Number in $number 
-	echo the pause is done, moving to the next loop 
+	echo Number in $number
+	echo the pause is done, moving to the next loop
 	echo ------
-done 
+done
 
 for seqfile in *.fasta
-do 
-	echo In $seqfile there are $(grep -c "." $seqfile) sequences
-	echo In this directory there are $(ls *.fasta | wc -l) fasta files  
-done 
+do
+	echo In "$seqfile" there are $(grep -c "." $seqfile) sequences
+	echo In this directory there are "$(ls *.fasta | wc -l)" fasta files
+done
 
 
